f6d1b6aff41c71943718e162ddab4510347648af
David A. Shamma <1846+ayman@users.noreply.github.com>
2020-07-04 12:20:34 -0700

    Adding menubar functions. Fixing constraints. Window is not resizable anymore.

--- PrettyJSON/AppDelegate.swift
+++ PrettyJSON/AppDelegate.swift
@@ -12,16 +12,15 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
-
+    @IBOutlet weak var openMenuItem: NSMenuItem!
+    @IBOutlet weak var closeMenuItem: NSMenuItem!
+    @IBOutlet weak var mainWindowMenuItem: NSMenuItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
