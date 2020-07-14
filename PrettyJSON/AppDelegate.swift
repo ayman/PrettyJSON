3e8a6356e3e2a06e64c825f3851431421476433c
David A. Shamma <1846+ayman@users.noreply.github.com>
2020-07-14 15:02:56 -0700

    Adding GPL headers.

--- PrettyJSON/AppDelegate.swift
+++ PrettyJSON/AppDelegate.swift
@@ -1,10 +1,26 @@
 //
-//  AppDelegate.swift
-//  PrettyJSON
+// AppDelegate.swift
+// PrettyJSON for Safari
 //
-//  Created by David A Shamma on 12/10/18.
-//  Copyright © 2018 shaumur.ai. All rights reserved.
-//
+// Created by David A Shamma on 12/10/18.
+// 
+// This file is part of the PrettyJSON for Safari distribution.
+// Copyright (c) 2020 David A. Shamma
+// https://github.com/ayman/PrettyJSON/
+// https://shamur.ai/bin/PrettyJSON/
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// 
 
 import Cocoa
 
